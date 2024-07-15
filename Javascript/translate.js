cd7ce947ca0f8673724cb04b4ad9be24b83e31f7
Anjali Kumawat <75383853+anjalikumawat2002@users.noreply.github.com>
2024-07-15 14:54:40 +0530

    Create translate.js

---
+++ Javascript/translate.js
@@ -0,0 +1,33 @@
+function authenticate() {
+  return gapi.auth2.getAuthInstance()
+      .signIn({scope: 'https://www.googleapis.com/auth/cloud-translation'})
+      .then(() => console.log('Sign-in successful'),
+            err => console.error('Error signing in', err));
+}
+
+function loadClient() {
+  gapi.client.setApiKey('YOUR_API_KEY');
+  return gapi.client.load('https://translation.googleapis.com/$discovery/rest?version=v3')
+      .then(() => console.log('GAPI client loaded for API'),
+            err => console.error('Error loading GAPI client for API', err));
+}
+
+function execute() {
+  const sourceText = document.getElementById('sourceText').value;
+  const targetLanguage = 'es';  // Replace with your target language code
+  return gapi.client.language.translations.translate({
+    'q': sourceText,
+    'target': targetLanguage
+  }).then(response => {
+    console.log('Response', response);
+    document.getElementById('output').innerText = response.result.data.translations[0].translatedText;
+  },
+  err => { 
+    console.error('Execute error', err);
+    document.getElementById('output').innerText = 'An error occurred: ' + err.message;
+  });
+}
+
+gapi.load('client:auth2', function() {
+  gapi.auth2.init({client_id: 'YOUR_CLIENT_ID'});
+});
