a895f7f64bbb6df85c34a1ef3bb0703b68f19f2f
Anjali Kumawat <75383853+anjalikumawat2002@users.noreply.github.com>
2024-07-15 15:02:49 +0530

    Update translate.js

--- Javascript/translate.js
+++ Javascript/translate.js
@@ -1,15 +1,15 @@
 function authenticate() {
   return gapi.auth2.getAuthInstance()
       .signIn({scope: 'https://www.googleapis.com/auth/cloud-translation'})
-      .then(() => console.log('Sign-in successful'),
-            err => console.error('Error signing in', err));
+      .then(() => console.log('Sign-in successful'))
+      .catch(err => console.error('Error signing in', err));
 }
 
 function loadClient() {
   gapi.client.setApiKey('YOUR_API_KEY');
   return gapi.client.load('https://translation.googleapis.com/$discovery/rest?version=v3')
-      .then(() => console.log('GAPI client loaded for API'),
-            err => console.error('Error loading GAPI client for API', err));
+      .then(() => console.log('GAPI client loaded for API'))
+      .catch(err => console.error('Error loading GAPI client for API', err));
 }
 
 function execute() {
@@ -21,8 +21,7 @@ function execute() {
   }).then(response => {
     console.log('Response', response);
     document.getElementById('output').innerText = response.result.data.translations[0].translatedText;
-  },
-  err => { 
+  }).catch(err => {
     console.error('Execute error', err);
     document.getElementById('output').innerText = 'An error occurred: ' + err.message;
   });
